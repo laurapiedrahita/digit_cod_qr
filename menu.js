85ec9f430331b12c06daf67c7fe101f96e52af64
laurapiedrahita <56948419+laurapiedrahita@users.noreply.github.com>
2019-10-23 22:56:52 -0500

    repo

---
+++ menu.js
@@ -0,0 +1,21 @@
+document.write('<body> \
+	 	<link rel="stylesheet" href="estilos.css" type="text/css" />\
+	    <h1><center>DigitCod QR.</center></h1>\
+	    <head>\
+			<title>Menu Desplegable</title> \
+		</head>\
+		<div id="header">\
+			<ul class="nav">\
+			    <li><a href="index.html">Home</a>\
+				</li>\
+				<li><a href="servicios.html">Servicios.</a>\
+				</li>\
+				<li><a href="historia.html">Historia.</a>\
+				</li>\
+				<li><a href="quienes.html">Quienes Somos.</a>\
+			     </li>\
+				<li><a href="contactenos.html">Contactenos.</a>\
+				</li>\
+			</ul>\
+		</div>\
+	</body>');
